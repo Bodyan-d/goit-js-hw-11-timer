cdf564b064bfb8c8008b77ee66797be13ec11dc5
Bogdan Dyachenko <46127718+Bodyan-d@users.noreply.github.com>
2021-05-22 21:33:46 +0300

    add-refs

---
+++ src/index.js
@@ -0,0 +1,16 @@
+const daysRef = document.querySelector('[data-value="days"]');
+const hoursRef = document.querySelector('[data-value="hours"]');
+const minsRef = document.querySelector('[data-value="mins"]');
+const secsRef = document.querySelector('[data-value="secs"]');
+
+const dateNow = Date.now();
+
+const days = Math.floor(time / (1000 * 60 * 60 * 24));
+const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+const mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
+const secs = Math.floor((time % (1000 * 60)) / 1000);
+
+new CountdownTimer({
+    selector: '#timer-1',
+    targetDate: new Date('Jul 17, 2019'),
+});
