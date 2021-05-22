bfb45567e0f634e03c53ae97499afec9d987d46f
Bogdan Dyachenko <46127718+Bodyan-d@users.noreply.github.com>
2021-05-22 22:34:26 +0300

    something

--- src/index.js
+++ src/index.js
@@ -3,14 +3,52 @@ const hoursRef = document.querySelector('[data-value="hours"]');
 const minsRef = document.querySelector('[data-value="mins"]');
 const secsRef = document.querySelector('[data-value="secs"]');
 
-const dateNow = Date.now();
 
-const days = Math.floor(time / (1000 * 60 * 60 * 24));
-const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-const mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
-const secs = Math.floor((time % (1000 * 60)) / 1000);
+class CountdownTimer {
+    constructor({ selector, targetDate, onTick }) {
+        this.selector = selector;
+        this.targetDate = targetDate;
+        this.intervalId = null;
+        this.onTick = onTick;
+    };
 
-new CountdownTimer({
+    start() {
+        const dateNow = Date.now();
+
+        this.intervalId = setInterval(() => {
+            const deltaTime = this.targetDate - dateNow;
+            const time = getTimeComponents(deltaTime);
+            this.onTick(time);
+
+        }, 1000);
+    };
+
+};
+
+const timer = new CountdownTimer({
     selector: '#timer-1',
-    targetDate: new Date('Jul 17, 2019'),
+    targetDate: new Date('Jul 17, 2021'),
+    onTick: updateClockface,
 });
+
+timer.start()
+
+function pad(velue) { return String(velue).padStart(2, '0') };
+
+function getTimeComponents(deltaTime) {
+
+
+    const days = pad(Math.floor(deltaTime / (1000 * 60 * 60 * 24)));
+    const hours = pad(Math.floor((deltaTime % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
+    const mins = pad(Math.floor((deltaTime % (1000 * 60 * 60)) / (1000 * 60)));
+    const secs = pad(Math.floor((deltaTime % (1000 * 60)) / 1000));
+
+    return { days, hours, mins, secs };
+};
+
+function updateClockface({ days, hours, mins, secs }) {
+    daysRef.textContent = days;
+    hoursRef.textContent = hours;
+    minsRef.textContent = mins;
+    secsRef.textContent = secs;
+};
